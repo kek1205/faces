4c96bb3a11706d7dfcb607bfc1422cf0514770f9
Eun Keum <e.keum1205@gmail.com>
2011-10-22 08:44:30 -0500

    modified:   app/views/welcome/index.html.erb 	new file:   config/application.rb 	new file:   config/boot.rb 	new file:   config/database.yml 	new file:   config/environment.rb 	new file:   config/environments/development.rb 	new file:   config/environments/production.rb 	new file:   config/environments/test.rb 	new file:   config/initializers/backtrace_silencers.rb 	new file:   config/initializers/inflections.rb 	new file:   config/initializers/mime_types.rb 	new file:   config/initializers/secret_token.rb 	new file:   config/initializers/session_store.rb 	new file:   config/initializers/wrap_parameters.rb 	new file:   config/locales/en.yml 	new file:   config/routes.rb 	new file:   db/seeds.rb 	new file:   lib/assets/.gitkeep 	new file:   lib/tasks/.gitkeep

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Faces::Application.config.secret_token = '26084fd73259e5e86f5991fdddcd9253f1a39d2eb284084f55f75549af3970e7a8ad7c9c2deb57200f2ca5b198f289236bfdc64f33889600465cf0a23b9bb2f3'
